06aa47d4c2df97f3119664b1c891fed7c13ed8ad
ritam <ritam.santra16@gmail.com>
2024-06-11 20:46:01 +0530

    This the Automobile file, including the inc, src and test

---
+++ MyLibrary/bulid/Automobile/CMakeFiles/Test_Car.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test_Car.dir/__/src/Vector2D.o"
+  "CMakeFiles/Test_Car.dir/__/src/Vector2D.o.d"
+  "CMakeFiles/Test_Car.dir/__/src/Vector3D.o"
+  "CMakeFiles/Test_Car.dir/__/src/Vector3D.o.d"
+  "CMakeFiles/Test_Car.dir/__/src/Vector3D_V2.o"
+  "CMakeFiles/Test_Car.dir/__/src/Vector3D_V2.o.d"
+  "CMakeFiles/Test_Car.dir/src/Bike.o"
+  "CMakeFiles/Test_Car.dir/src/Bike.o.d"
+  "CMakeFiles/Test_Car.dir/src/Car.o"
+  "CMakeFiles/Test_Car.dir/src/Car.o.d"
+  "CMakeFiles/Test_Car.dir/src/Truck.o"
+  "CMakeFiles/Test_Car.dir/src/Truck.o.d"
+  "CMakeFiles/Test_Car.dir/src/Vehicle.o"
+  "CMakeFiles/Test_Car.dir/src/Vehicle.o.d"
+  "CMakeFiles/Test_Car.dir/test/Test_Car.o"
+  "CMakeFiles/Test_Car.dir/test/Test_Car.o.d"
+  "Test_Car"
+  "Test_Car.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test_Car.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
